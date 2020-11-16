decd031ffc2d254a38d8441f3768e8a33444a762
Eric Hippler <hippler.eric@gmail.com>
2020-11-16 13:49:09 -0500

    Basic layout created

--- capstone/maintain/views.py
+++ capstone/maintain/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from maintain.helpers import call_car_md, get_credits
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hey, the maintain app")
+    return render(request, "maintain/index.html")
