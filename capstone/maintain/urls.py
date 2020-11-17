84c73c23e1729216d6fbfaefe40820fe8d87c6e9
Eric Hippler <hippler.eric@gmail.com>
2020-11-16 19:42:52 -0500

    Working login, register, logout

--- capstone/maintain/urls.py
+++ capstone/maintain/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("register", views.register_view, name="register"),
+    path("logout", views.logout_view, name="logout"),
 ]
--- capstone/maintain/views.py
+++ capstone/maintain/views.py
@@ -1,7 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+
+from .models import User
 
 # Create your views here.
 
+@login_required(login_url='login')
 def index(request):
     return render(request, "maintain/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse(index))
+        else:
+            return render(request, "maintain/login.html", {
+                "alert": "warning",
+                "alert_message": "Invalid username or password"
+            })
+    else:
+        return render(request, "maintain/login.html")
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "maintain/register.html", {
+                "alert": "warning",
+                "alert_message": "Passwords must match"
+            })
+
+        # Create new user
+        try:
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+        except IntegrityError:
+            return render(request, "maintain/register.html", {
+                "alert": "warning",
+                "alert_message": "Username not available"
+            })
+        login(request, user)
+        return redirect(reverse("index"))
+    else:
+        return render(request, "maintain/register.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse(index))
